4fe73709a02d60851b93fcc0eb84289bf633a9c7
Dora <939217129@qq.com>
2022-01-17 11:55:23 +0800

    deploy

---
+++ precache-manifest.36890f48bb8801be9eb93a418a99c858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd85b8aae6fbe794ec64",
+    "url": "/living-website/css/app.6a02c428.css"
+  },
+  {
+    "revision": "b19c77dc2f6d5ea09f5d2e8098724ab2",
+    "url": "/living-website/index.html"
+  },
+  {
+    "revision": "cd85b8aae6fbe794ec64",
+    "url": "/living-website/js/app.c28386db.js"
+  },
+  {
+    "revision": "933a47f1a3437e051a80",
+    "url": "/living-website/js/chunk-vendors.81df0a24.js"
+  },
+  {
+    "revision": "67360c6d75be4b08807db5abf4ecf3ff",
+    "url": "/living-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/living-website/robots.txt"
+  }
+]);
